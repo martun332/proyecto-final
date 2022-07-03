914fe090fcac0f2e477260fb0d3317520f76855c
Martin Baez <martin.baez@live.com.ar>
2022-07-03 14:00:00 -0300

    Salto desde pagina a pagina

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,6 @@ from django.urls import include, path
 from .views import inicio, nosotros
 
 urlpatterns = [
-    path('', inicio),
-    path('nosotros/', nosotros),
+    path('', inicio, name='pagina_index'),
+    path('nosotros/', nosotros, name='pagina_nosotros'),
 ]
